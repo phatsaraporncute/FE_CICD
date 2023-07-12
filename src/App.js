1605e0232835bc1dfd38799e9c6858a9178bf6d1
phatsaraporncute <phatsaraaaa@gmail.com>
2023-07-12 19:25:27 +0700

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target='_blank'
                     rel='noopener noreferrer'
                 >
-                    Leaaarnn
+                    Leaaarnn DevOppppps
                 </a>
                 {/* <img src="https://images.unsplash.com/photo-1689028427588-508115d0b6a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3032&q=80" /> */}
             </header>
